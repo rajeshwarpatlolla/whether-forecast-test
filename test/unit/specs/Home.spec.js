656f6a0e358028b58d938109a6442e6605c5c8f1
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2017-05-28 16:16:17 +0530

    added minor changes

--- test/unit/specs/Home.spec.js
+++ test/unit/specs/Home.spec.js
@@ -2,13 +2,15 @@ import Vue from 'vue';
 import { Home } from '../../../src/components';
 
 describe('Home.vue', () => {
-  it('should render correct type', () => {
+  it('should show the type of view selected', () => {
     const Constructor = Vue.extend(Home);
     const vm = new Constructor().$mount();
     expect(vm.$el.querySelector('.hello h2').textContent).to.equal('graph');
   });
 
-  it('should render correct value currentIndex', () => {
-
-  });
+  // it('should result in correct text about hide button clicked', () => {
+  //   const Constructor = Vue.extend(Home);
+  //   const vm = new Constructor().$mount();
+  //   expect(vm.$el('#city_name')).toHaveText('Moscow');
+  // });
 });
