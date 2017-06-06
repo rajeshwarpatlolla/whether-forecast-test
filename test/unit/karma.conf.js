e1922015adc642d0489884ecfc6d3b114154bcc6
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2017-06-07 00:09:26 +0530

    test case framework modified

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -3,31 +3,24 @@
 // we are also using it with karma-webpack
 //   https://github.com/webpack/karma-webpack
 
-var webpackConfig = require('../../build/webpack.test.conf');
+const webpackConfig = require('../../build/webpack.test.conf');
 
 module.exports = function (config) {
   config.set({
-    // to run in additional browsers:
-    // 1. install corresponding karma launcher
-    //    http://karma-runner.github.io/0.13/config/browsers.html
-    // 2. add it to the `browsers` array below.
     browsers: ['PhantomJS'],
-    frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
-    reporters: ['spec', 'coverage'],
-    files: ['./index.js'],
+    frameworks: ['jasmine'],
+    // this is the entry file for all our tests.
+    files: ['index.js'],
+    // we will pass the entry file to webpack for bundling.
     preprocessors: {
-      './index.js': ['webpack', 'sourcemap']
+      'index.js': ['webpack'],
     },
+    // use the webpack config
     webpack: webpackConfig,
+    // avoid walls of useless text
     webpackMiddleware: {
       noInfo: true,
     },
-    coverageReporter: {
-      dir: './coverage',
-      reporters: [
-        { type: 'lcov', subdir: '.' },
-        { type: 'text-summary' },
-      ]
-    },
+    singleRun: true,
   });
 };
--- test/unit/specs/Home.spec.js
+++ test/unit/specs/Home.spec.js
@@ -1,25 +1,83 @@
 import Vue from 'vue';
 import { Home } from '../../../src/components';
 
-describe('Home.vue', () => {
-  it('should show the type of view selected', () => {
-    const Constructor = Vue.extend(Home);
-    const vm = new Constructor().$mount();
-    expect(vm.$el.querySelector('.hello h2').textContent).to.equal('graph');
-  });
-
-  it('sets the correct default data', () => {
+describe('variables', () => {
+  it('sets the correct current index', () => {
     const defaultData = Home.data();
     expect(defaultData.currentIndex).toBe(0);
   });
 
-  it('emitter has should be a function', () => {
+  it('sets the correct dataSet', () => {
+    const defaultData = Home.data();
+    expect(defaultData.dataSet.length).toBe(0);
+  });
+
+  it('sets the correct temperaturList', () => {
+    const defaultData = Home.data();
+    expect(defaultData.temperaturList.length).toBe(0);
+  });
+
+  it('sets the correct temperaturListArr', () => {
+    const defaultData = Home.data();
+    expect(defaultData.temperaturListArr.length).toBe(0);
+  });
+
+  it('sets the correct mapUrl', () => {
+    const defaultData = Home.data();
+    expect(defaultData.mapUrl).toBe(null);
+  });
+
+  it('sets the correct whetherData', () => {
+    const defaultData = Home.data();
+    expect(defaultData.whetherData).toEqual({
+      city: null,
+      cnt: null,
+      cod: null,
+      list: [],
+      weather: [{ icon: null }],
+    });
+  });
+
+  it('sets the correct view Type', () => {
+    const defaultData = Home.data();
+    expect(defaultData.viewType).toBe('graph');
+  });
+});
+
+describe('methods', () => {
+  it('should have a function with the name data', () => {
+    expect(typeof Home.data).toBe('function');
+  });
+
+  it('should have a function with the name mounted', () => {
+    expect(typeof Home.mounted).toBe('function');
+  });
+
+  it('should have a function with the name changeView', () => {
     expect(typeof Home.methods.changeView).toBe('function');
   });
 
-  // it('should result in correct text about hide button clicked', () => {
-  //   const Constructor = Vue.extend(Home);
-  //   const vm = new Constructor().$mount();
-  //   expect(vm.$el('#city_name')).toHaveText('Moscow');
-  // });
+  it('should have a function with the name getWHetherDetails', () => {
+    expect(typeof Home.methods.getWHetherDetails).toBe('function');
+  });
+
+  it('should have a function with the name prepareData', () => {
+    expect(typeof Home.methods.prepareData).toBe('function');
+  });
+
+  it('should have a function with the name removeHMS', () => {
+    expect(typeof Home.methods.removeHMS).toBe('function');
+  });
+
+  it('should have a function with the name getit', () => {
+    expect(typeof Home.methods.getit).toBe('function');
+  });
+});
+
+describe('rendered content', () => {
+  it('should show the type of view selected', () => {
+    const Constructor = Vue.extend(Home);
+    const vm = new Constructor().$mount();
+    expect(vm.$el.querySelector('.hello h2').textContent).toBe('graph');
+  });
 });
